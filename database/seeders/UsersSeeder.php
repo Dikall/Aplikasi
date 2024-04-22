629c30a0991efac893b401a9f693018398405a2b
Pheterson Ferry Fernando <phetersonnnz@gmail.com>
2024-04-22 18:18:29 +0700

    First Commit

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,24 +2,17 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class DatabaseSeeder extends Seeder
+class UsersSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-            
         User::create([
             'name'=>'Mahasiswa',
             'email'=>'mahasiswa@email.com',
@@ -110,5 +103,5 @@ class DatabaseSeeder extends Seeder
             'type'=>12,
             'password'=> bcrypt('12345'),
         ]);
-}
+    }
 }
